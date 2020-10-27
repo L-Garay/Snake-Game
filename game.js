768f3dbadc34bd28a8b71883146a1766466194a0
Logan Garay <garay.logan@gmail.com>
2020-10-27 15:17:39 -0600

    rendering basic snake and hardcoded movement

---
+++ game.js
@@ -0,0 +1,38 @@
+import {
+  snakeSpeed,
+  draw as drawSnake,
+  update as updateSnake,
+} from './snake.js';
+
+// create variable to hold location of where we want the game to be
+const gameBoard = document.getElementById('game-board');
+
+// NOTE Create a game-loop method to repeatedly run to essentially constantly update the game and re-render the screen
+
+// create variable that will hold when the game was last rendered
+let lastRenderTime = 0;
+
+function main(currentTime) {
+  window.requestAnimationFrame(main);
+  // divide by 1000 to turn into seconds
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+  // if the time since last render is less than how quickly the snake moves (in this case 1/2 = .5, so every half a second) than we don't need to rerender and we can return out of function
+  if (secondsSinceLastRender < 1 / snakeSpeed) return;
+  // if the time since last render is greater than how quicly the sname moves, reset the last render time and then they cycle will repeat
+  lastRenderTime = currentTime;
+  update();
+  draw();
+}
+
+// Actually run the game
+window.requestAnimationFrame(main);
+
+function update() {
+  updateSnake();
+}
+
+function draw() {
+  // clear out any previous positions of where the snake used to be (the proper positions will get rerendered)
+  gameBoard.innerHTML = '';
+  drawSnake(gameBoard);
+}
---
+++ snake.js
@@ -0,0 +1,39 @@
+// set how quickly snake will move a second
+export const snakeSpeed = 2;
+
+// create the snake 'body' to start at the center of board
+const snakeBody = [
+  { x: 11, y: 11 },
+  { x: 12, y: 11 },
+  { x: 13, y: 11 },
+  { x: 14, y: 11 },
+  { x: 15, y: 11 },
+  { x: 16, y: 11 },
+  { x: 17, y: 11 },
+];
+
+export function update() {
+  // this for loop starts at the second to last body part of the snake, and makes its way to the head
+  for (let i = snakeBody.length - 2; i >= 0; i--) {
+    // the first loop will grab the last body part of the snake and essentially 'moves' it to where the second to last body part is (creates a new object in the process)
+    snakeBody[i + 1] = { ...snakeBody[i] };
+  }
+  // then we must update the head based on the user's input
+  snakeBody[0].x += 0;
+  snakeBody[0].y += 1;
+}
+
+// Method takes in the gameBoard, the 21x21 grid located in the div with the id of 'game-board'
+export function draw(gameBoard) {
+  snakeBody.forEach((segment) => {
+    // this will create the first block of the snake body using a div element
+    const snakeElement = document.createElement('div');
+    // ignore the errors, these tell the newly created div element where to render (in this case we have predetermined it to be in the center of the grid)
+    snakeElement.style.gridRowStart = segment.y;
+    snakeElement.style.gridColumnStart = segment.x;
+    // apply the css class we created in the html file to this newly created div
+    snakeElement.classList.add('snake');
+    // add the newly created and styled div element to the 21x21 css grid div element called 'game-board' that was passed into the method
+    gameBoard.appendChild(snakeElement);
+  });
+}
